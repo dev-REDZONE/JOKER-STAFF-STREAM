c09b16acdd1adae9942d1a51fa53c323b8655755
dev-REDZONE <78139545+dev-REDZONE@users.noreply.github.com>
2021-07-16 16:37:52 +0300

    Update and rename Corona.js to Redzone.js

--- Redzone.js
+++ Redzone.js
@@ -1,27 +1,27 @@
 const Discord = require("discord.js");
-const Yaxi = new Discord.Client();
+const redzone = new Discord.Client();
 //////// playing or dnd  streaming ////////
-Yaxi.on("ready", () => {
+redzone.on("ready", () => {
   console.log(
     `Online In Servers`
   );
   let statuses = [
   
-  `You only live once, so try to live for God.`,
+  `JOKER STAFF UP`,
  
   ];
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
-    corona.user.setActivity(STREAMING, {      
+    redzone.user.setActivity(STREAMING, {      
     type: "STREAMING", 
     url: "https://www.twitch.tv/faith"
     });
   }, 2000);
 });
 ///////////////////////
-corona.on("ready", () => {
-    var join = corona.channels.get(""); /// id voice
+redzone.on("ready", () => {
+    var join = redzone.channels.get(""); /// id voice
     if (join) join.join();
   });
 //////////////// tokn lera da bne 
-corona.login("");
+redzone.login("");
